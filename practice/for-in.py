976d752f91087064c8450e84a1c2c44e4cba589a
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-04-17 13:50:11 +0200

    Update for-in.py

--- practice/for-in.py
+++ practice/for-in.py
@@ -1,3 +1,10 @@
-my_string = "this is a test string"
+my_string   = " this is a test string "
+my_list     = [" this is", " ", "a test ", "list" ]
 
-print([x.strip() for x in my_string])
+#print([x.strip() for x in my_string]) #oneliner
+
+def my_func(x):
+    print([y.strip() for y in x]) #oneliner
+
+my_func(my_string)
+my_func(my_list)
