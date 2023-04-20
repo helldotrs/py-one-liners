97923338257ec780639bb1034c93c48f59421d8c
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-04-20 10:03:07 +0200

    Create example3-1.py

---
+++ ch03/example3-1.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# 2d array from list of lists
+b = np.array( [
+                [1, 2],
+                [3, 4]
+              ])
+
+print(b)
+
+# 3d array from list of lists of lists
+c = np.array( [
+                [
+                  [1, 2],
+                  [3, 4]
+                ][
+                  [5, 6],
+                  [7, 8]
+                ]
+              ])
+
+print(c)
