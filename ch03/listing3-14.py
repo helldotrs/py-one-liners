266c08fd52ffb3a6f2c611b864c4885201467ff9
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-04-25 22:05:36 +0200

    Create listing3-14.py

---
+++ ch03/listing3-14.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#data, rows --> cities
+x np.array(
+  [
+    [42, 40, 42, 43, 44, 43], #HK
+    [30, 30, 29, 30, 31, 30], #NY
+    [8, 11, 13, 11, 11, 9], #Berlin
+    [11, 12, 10, 11, 13, 22], #Montreal
+  ]
+)
+
+# one-liner:
+polluted = set(cities[np.nonzero(x < np.average(x))[0]]) #fr how cool is  this line??!
+
+print(polluted)
+    
